1e91890b79998f71965b9818c53ca3d21ef29b18
Rafael Matos <rafaeldevcomsono@outlook.com>
2024-05-16 22:18:42 -0300

    Testando primeiro commit

---
+++ Controllers/PawSolidarityController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjetoPataSolidaria.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PawSolidarityController : ControllerBase
+    {
+
+
+        public PawSolidarityController()
+        {
+
+        }
+
+        [HttpGet("")]
+        public void Get()
+        {
+
+        }
+    }
+}
